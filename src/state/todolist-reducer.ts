94ffdc30fd0eda1679394e51b489d6e5a143d900
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-25 14:02:49 +0300

    feat: add reducer

--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -1,11 +1,14 @@
 import {TodolistType} from "../App";
 import { v1 } from 'uuid'
-type ActionType = removeTodolistACType | addTodoListACType;
-type StateType = any;
+type ActionType = removeTodolistACType
+    | addTodoListACType
+    | changeTodoLIstTitleACType;
+
 type ReducerType<T1,T2> = (s:T1, a:T2) => T1
 
 type removeTodolistACType = ReturnType<typeof removeTodolistAC>
 type addTodoListACType =  ReturnType<typeof addTodoListAC>
+type changeTodoLIstTitleACType =  ReturnType<typeof changeTodoLIstTitleAC>
 
 export const removeTodolistAC = (id: string) => ({
     type: 'REMOVE-TODOLIST',
@@ -19,12 +22,22 @@ export const addTodoListAC = (title: string) => ({
         title
     }
 } as const)
+export const changeTodoLIstTitleAC = (id: string, title: string) => (
+    {
+        type: 'CHANGE-TODOLIST-TITLE',
+        payload: {
+            id, title
+        }
+    } as const
+)
 export const todolistReducer:ReducerType<TodolistType[], ActionType> = (state, action) => {
     switch(action.type){
         case 'REMOVE-TODOLIST':
             return state.filter(e => e.id !== action.payload.id)
         case 'ADD-TODOLIST' :
             return [...state, {id: v1(), title: action.payload.title, filter: 'all' }]
+        case 'CHANGE-TODOLIST-TITLE':
+            return state.map( e => e.id === action.payload.id ? {...e, title: action.payload.title} : e)
         default:
             throw new Error('Unknown action type')
     }
