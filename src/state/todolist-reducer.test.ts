a1a0dfc5ca56e9b37d674a2a5c62b8f2787377a9
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-25 14:02:57 +0300

    feat: add tdd

--- src/state/todolist-reducer.test.ts
+++ src/state/todolist-reducer.test.ts
@@ -1,6 +1,6 @@
-import {addTodoListAC, removeTodolistAC, todolistReducer} from './todolist-reducer'
-import { v1 } from 'uuid'
-import { TodolistType } from '../App'
+import {addTodoListAC, changeTodoLIstTitleAC, removeTodolistAC, todolistReducer} from './todolist-reducer'
+import {v1} from 'uuid'
+import {TodolistType} from '../App'
 
 test('correct todolist should be removed', () => {
     let todolistId1 = v1()
@@ -30,4 +30,27 @@ test('correct todolist should be added', () => {
 
     expect(endState.length).toBe(3)
     expect(endState[2].title).toBe(newTodolistTitle)
+
+})
+test('correct todolist should change its name', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let newTodolistTitle = 'New Todolist'
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    const action = {
+        type: 'CHANGE-TODOLIST-TITLE',
+        id: todolistId2,
+        title: newTodolistTitle
+    }
+
+    const endState = todolistReducer(startState, changeTodoLIstTitleAC(todolistId2, newTodolistTitle))
+
+    expect(endState[0].title).toBe('What to learn')
+    expect(endState[1].title).toBe(newTodolistTitle)
 })
