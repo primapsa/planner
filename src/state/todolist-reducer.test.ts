068cf57c040f43ffbd3a38535dad8eb5c8038dcb
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-25 13:43:00 +0300

    feat: add tdd

--- src/state/todolist-reducer.test.ts
+++ src/state/todolist-reducer.test.ts
@@ -1,4 +1,4 @@
-import {removeTodolistAC, todolistReducer} from './todolist-reducer'
+import {addTodoListAC, removeTodolistAC, todolistReducer} from './todolist-reducer'
 import { v1 } from 'uuid'
 import { TodolistType } from '../App'
 
@@ -14,4 +14,20 @@ test('correct todolist should be removed', () => {
     const endState = todolistReducer(startState, removeTodolistAC(todolistId1))
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todolistId2)
+})
+test('correct todolist should be added', () => {
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let newTodolistTitle = 'New Todolist'
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    const endState = todolistReducer(startState, addTodoListAC(newTodolistTitle))
+
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodolistTitle)
 })
