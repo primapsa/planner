eab5d6b9798decf32a61c39412327ee9a8c68f36
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-25 13:43:13 +0300

    feat: add reducer

--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -1,11 +1,11 @@
 import {TodolistType} from "../App";
-
-type ActionType = removeTodolistACType;
+import { v1 } from 'uuid'
+type ActionType = removeTodolistACType | addTodoListACType;
 type StateType = any;
 type ReducerType<T1,T2> = (s:T1, a:T2) => T1
 
 type removeTodolistACType = ReturnType<typeof removeTodolistAC>
-
+type addTodoListACType =  ReturnType<typeof addTodoListAC>
 
 export const removeTodolistAC = (id: string) => ({
     type: 'REMOVE-TODOLIST',
@@ -13,11 +13,18 @@ export const removeTodolistAC = (id: string) => ({
       id
     }
 } as const)
-
+export const addTodoListAC = (title: string) => ({
+    type: 'ADD-TODOLIST',
+    payload: {
+        title
+    }
+} as const)
 export const todolistReducer:ReducerType<TodolistType[], ActionType> = (state, action) => {
     switch(action.type){
         case 'REMOVE-TODOLIST':
             return state.filter(e => e.id !== action.payload.id)
+        case 'ADD-TODOLIST' :
+            return [...state, {id: v1(), title: action.payload.title, filter: 'all' }]
         default:
             throw new Error('Unknown action type')
     }
