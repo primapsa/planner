267081697c4933226eb751c7baf25a4c59592c73
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-25 13:29:32 +0300

    feat: add tdd

--- src/state/user-reducer.test.ts
+++ src/state/user-reducer.test.ts
@@ -11,5 +11,14 @@ test('user reducer should increment only age', () => {
 
 test('user reducer should increment only childrenCount', () => {
     const startState = {age: 20, childrenCount: 2, name: 'Dimych'}
-    // your code here
+    const endState = userReducer(startState, {type: 'INCREMENT-CHILDREN-COUNT'})
+    expect(startState.childrenCount).toBe(2)
+    expect(endState.childrenCount).toBe(3)
+})
+test('user reducer should change name of user', () => {
+    const startState = {name: 'Dimych', age: 20, childrenCount: 2}
+    const newName = 'Viktor'
+    const endState = userReducer(startState, {type: 'CHANGE-NAME', newName: newName})
+
+    expect(endState.name).toBe(newName)
 })
