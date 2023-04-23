78a1c8342a1a5778671f183a3200cc6efa6d0401
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-23 21:40:56 +0300

    feat: add  reducer tdd

---
+++ src/state/todolists-tasks-reducer.test.ts
@@ -0,0 +1,20 @@
+import {TasksStateType, TodolistType} from "../App";
+import {tasksReducer} from "./task-reducer";
+import {addTodoListAC, todolistReducer} from "./todolist-reducer";
+
+test('ids should be equals', () => {
+    const startTasksState: TasksStateType = {}
+    const startTodolistsState: Array<TodolistType> = []
+
+    const action = addTodoListAC('new todolist')
+
+    const endTasksState = tasksReducer(startTasksState, action)
+    const endTodolistsState = todolistReducer(startTodolistsState, action)
+
+    const keys = Object.keys(endTasksState)
+    const idFromTasks = keys[0]
+    const idFromTodolists = endTodolistsState[0].id
+
+    expect(idFromTasks).toBe(action.payload.id)
+    expect(idFromTodolists).toBe(action.payload.id)
+})
