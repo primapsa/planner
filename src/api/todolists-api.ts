6cdee76ecb5b5206b109064ff0bd20504cfe61d7
Siarhei Prymakovich <prima.psa@gmail.com>
2023-07-06 22:36:44 +0300

    refactor: api key

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': '480625ae-d064-4dea-bab6-f5827c8f276a'
     }
 })
 
